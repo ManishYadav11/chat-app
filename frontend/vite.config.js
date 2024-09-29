bd265e8a3bced069ae7c474800bf91ef012b38f6
Manish Yadav <117844262+ManishYadav11@users.noreply.github.com>
2024-09-30 01:45:13 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:8000",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000", // Use this target for API requests
+        changeOrigin: true, // Optionally set to true to handle CORS issues
+        rewrite: (path) => path.replace(/^\/api/, ""), // Optional: removes "/api" prefix
+      },
+    },
+  },
 });
